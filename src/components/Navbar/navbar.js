5dd2582f9e1e5491e99d3145785b91d9aa5824f4
Clementine <clemwanjiru@gmail.com>
2024-01-25 14:18:12 +0300

    lets go

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../../assests/logo4.png";
+import contactImg from "../../assests/call-removebg-preview.png";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="logo" className="logo" />
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">About</Link>
+        <Link className="desktopMenuListItem">Portfolio</Link>
+        <Link className="desktopMenuListItem">Client</Link>
+      </div>
+      <button className="desktopMenuBtn">
+        <img src={contactImg} alt="" className="desktopMenuImg" />
+        Contact Me
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
