c271c5f082497041e1271b4f71457df2a1d532bf
Clementine <clemwanjiru@gmail.com>
2024-01-30 23:10:00 +0300

    about

---
+++ src/components/Skills/skills.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import  './skills.css';
+
+const skills = () => {
+  return (
+    <section id="skills">
+        THIS IS THE ABOUT SECTION
+    </section>
+  )
+}
+
+export default skills
